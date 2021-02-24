75de5116c29be4330e7ea033c27ef6e638ba37a7
dcouch440 <dcouch440@gmail.com>
2021-02-24 14:52:26 -0800

    add check input and method, add computer logic function
    Co-authored-by: Kerry Lang <klang812@gmail.com>
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,5 +1,36 @@
+#!/usr/bin/env ruby
 class RPS
+  def initialize()
+    @actions = ['rock', 'scissors', 'paper']
+  end
   def wins?(input_one, input_two)
-    nil
+    win_conditions = [
+      ['rock', 'scissors'],
+      ['scissors', 'paper'],
+      ['paper', 'rock']
+    ]
+    win_conditions.each_with_index do |win_condition, winner_index|
+      a,b = win_condition.sort.map(&:downcase)
+      c,d = [input_one, input_two].sort.map(&:downcase)
+      winner = win_conditions[winner_index][0].capitalize
+      if check_input?(input_one)
+        return "Wrong input! FIX IT!"
+        break
+      elsif "#{a} #{b}" == "#{c} #{d}"
+        return "#{winner} is the winner!"
+        break
+      elsif "#{c}" == "#{d}"
+        return "uh oh! you picked the same things"
+        break
+      end
+    end
+  end
+  def check_input?(input_one)
+    !@actions.include?(input_one)
+  end
+  def computer
+    @actions.sample
   end
 end
+game = RPS.new()
+puts game.wins?('rock', game.computer)
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -6,7 +6,25 @@ describe('RPS') do
   describe("#wins") do 
     it("returns true if rock is the object and scissors is the argument") do
       game = RPS.new()
-      expect(game.wins?("rock", "scissors")).to(eq(true))
+      expect(game.wins?("rock", "scissors")).to(eq("Rock is the winner!"))
+    end
+    it("returns true if scissors is the object and paper is the argument") do
+      game = RPS.new()
+      expect(game.wins?("scissors", "paper")).to(eq("Scissors is the winner!"))
+    end
+    it("returns true if paper is the object and rock is the argument") do
+      game = RPS.new()
+      expect(game.wins?("paper", "rock")).to(eq("Paper is the winner!"))
+    end
+    it("returns a promp if the players chose the same thing") do
+      game = RPS.new()
+      expect(game.wins?("rock", "rock")).to(eq("uh oh! you picked the same things"))
+    end
+  end
+  describe("#check_input") do
+    it("checking user input to make sure its valid input") do
+      game = RPS.new()
+      expect(game.check_input?("x")).to(eq(false))
     end
   end
 end
